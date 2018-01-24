ebdec045058793c289beb819786ed7b0a390726d
4ndr3wch4n <chan.andrew1995@gmail.com>
2018-01-24 13:49:20 -0500

    Completed barebones portfolio

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#fullpage').fullpage(
     {
       anchors:['Intro', 'About', 'Projects'],
-      sectionsColor: ['black','#2e2e33','black'],
+      sectionsColor: ['black','#2e2e33','black', '#2e2e33'],
       navigation: 'true',
 			navigationPosition: 'top',
     }
