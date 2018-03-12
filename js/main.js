3060753e806091e710b3873d023511ae8fa706f8
4ndr3wch4n <chan.andrew1995@gmail.com>
2018-03-12 12:43:12 -0400

    Updated so that scrolling is not longer fullpage when under 992 px

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
       sectionsColor: ['black','#2e2e33','black', '#2e2e33'],
       navigation: 'true',
 			navigationPosition: 'top',
+			scrollOverflow: true,
+			responsiveWidth: 992
     }
   );
 });
